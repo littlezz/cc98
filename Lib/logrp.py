7a3cb309bd495bf3a329ef059a0bc47273f03e8c
zz <zz.at.field@gmail.com>
2014-04-16 17:30:39 +0800

    hi~

---
+++ Lib/logrp.py
@@ -0,0 +1,83 @@
+import httplib2 as ht2
+#from urllib.parse import urlencode
+#from urllib.parse import urlparse
+#import urllib.parse
+from urllib.parse import *
+import urllib.parse
+from bs4 import BeautifulSoup as bs
+import hashlib
+import random,os
+
+
+print(os.getcwd())
+rpbase="http://www.cc98.org/SaveReAnnounce.asp?"
+with open("./data/resp.txt",encoding="utf-8") as f:
+    contents=[]
+    for i in f:
+        contents.append(i.rstrip())
+
+
+def login(username,passwd,url,head):
+    
+    passwd=hashlib.md5(passwd.encode()).hexdigest()
+    
+    PostForm=(('a','i'),('u',username),('p',passwd),('userhidden','2'))
+    PostForm=urlencode(PostForm)
+    
+    h=ht2.Http('.cache')
+    req,u=h.request(url,"POST",headers=head,body=PostForm)
+    headcp=head.copy()
+    s=headcp['Cookie']=req['set-cookie']
+    pd=s.split("password=")[1].split(";")[0]
+    print('ok')
+    headcp.pop('X-Requested-With')
+    return headcp,pd
+
+def req(url,head):
+   
+    res,u=h.request(url,headers=head)
+    print(url)
+    u=bs(u)
+    fu=u.find(attrs={"name":"followup"}).get("value")
+    BdidRtid=urllib.parse.parse_qsl(urlparse(url).query)
+    mt=('method','fastreply')
+    bdid=('BoardID',BdidRtid[0][1])
+    dt=(mt,bdid)
+    reurl=rpbase+urlencode(dt)
+    print(reurl)
+    print('ok')
+    return reurl,BdidRtid[1][1],fu
+
+def reply(head,url,pd,rtid,fu,username):
+    fu=('followup',str(fu))
+    rtid=('RootID',rtid)
+    star=('star','1')
+    user_name=('UserName',username)
+    pswd=('passwd',pd)
+    ep=("Expression","face7.gif")
+    content=("Content",random.choice(contents))
+    
+    sn=("signflag","yes")
+    contentForm=(fu,rtid,star,user_name,pswd,ep,content,sn)
+    
+    print(urlencode(contentForm))
+    print(random.choice(contents))
+    h=ht2.Http('.cache')
+    req,u=h.request(url,"POST",headers=head,body=urlencode(contentForm))
+    print(req)
+    
+    print('rep')
+    
+"""    
+#
+ht2.debuglevel=1
+with open('head.pickle','rb')as f:
+    head=pickle.load(f)
+#
+"""
+h=ht2.Http('.cache')
+
+    
+
+    
+    
---
+++ cc98.py
@@ -0,0 +1,49 @@
+#!python3
+
+
+"""
+passwd==password
+pd==passwd (in Cookie)
+
+no cookie record Ver
+"""
+
+import Lib.logrp as logrp
+import pickle
+import os
+os.chdir('data')
+
+with open('username.txt',encoding="utf-8") as f:
+    usernames=[]
+    for i in f:
+        usernames.append(i.rstrip())
+
+with open('password.txt',encoding="utf-8") as f:
+    passwds=[]
+    for i in f:
+        passwds.append(i.rstrip())
+
+with open('head.pickle','rb')as f:
+    loghead=pickle.load(f)
+
+
+url=input()
+rpNum=int(input("用多少个马甲\n"))
+rpNum=min(rpNum,len(passwds))
+logurl='http://www.cc98.org/sign.asp'
+
+for i in range(rpNum):
+    username=usernames[i]
+    passwd=passwds[i]
+
+    head,pd=logrp.login(username,passwd,logurl,loghead)
+    head["Accept"]="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    head["Content-Type"]="application/x-www-form-urlencoded"
+    
+    reurl,rtid,fu=logrp.req(url,head)
+    logrp.reply(head,reurl,pd,rtid,fu,username)
+    print('over')
+
+    
+
+
